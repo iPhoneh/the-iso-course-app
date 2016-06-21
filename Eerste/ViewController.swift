56d48e98afdbac3c103fec8946952f826da440a6
Sabir el Fassi <Sabir@sabirs-macbook-pro.local>
2016-06-21 21:05:33 +0200

    Initial Commit

---
+++ Eerste/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eerste
+//
+//  Created by Sabir el Fassi on 21/06/16.
+//  Copyright © 2016 Sabir el Fassi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
