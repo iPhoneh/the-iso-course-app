0247946bcd2abd91e8b02c046ea3c247d29575ba
Sabir el Fassi <Sabir@Sabirs-MacBook-Pro.local>
2016-06-21 23:58:20 +0200

    even snel geadd

--- Eerste/ViewController.swift
+++ Eerste/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var bluebird: UIImageView!
+    @IBOutlet weak var redbird: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func hideblue(sender: AnyObject) {
+        bluebird.hidden = true
+    }
+    @IBAction func hidered(sender: AnyObject) {
+        redbird.hidden = true
     }
 
 
